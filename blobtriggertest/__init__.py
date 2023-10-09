d557023abe0caa2f53cd33f858f1eecc2b3bc99d
StormChaser-infinite <105186977+StormChaser-infinite@users.noreply.github.com>
2023-10-09 21:11:56 +1300

    tested the continuous deployment in the Azure portal

--- blobtriggertest/__init__.py
+++ blobtriggertest/__init__.py
@@ -227,6 +227,7 @@ def export_ouput(df_output):
 
 
 def main(myblob: func.InputStream):
+    '''calculate the mrc'''
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n")
     
